d303cd69dd51aa45c4c9af0b47c6f7308d24d871
Jaime Marquínez Ferrándiz <jaimemf93@gmail.com>
2013-02-23 21:25:08 +0100

    New example urls

--- urlform.js
+++ urlform.js
@@ -1,6 +1,4 @@
-function setSteamURL(form) {
-	URLform.url.value='http://store.steampowered.com/video/105600/';
-}
-function setUstreamURL(form) {
-	URLform.url.value='http://www.ustream.tv/recorded/20274954';
+function setURL(form,url) {
+	//Set the url field of the form
+	URLform.url.value=url;
 }
